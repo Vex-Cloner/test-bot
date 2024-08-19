a18e1937f62dd113cb73688c1af2ede4e0225d8e
Vex-Cloner <npanimexyt@gmail.com>
2024-08-19 15:22:54 +0545

    Update login.py

--- login.py
+++ login.py
@@ -1,23 +1,20 @@
 from fbchat import Client
 import json
-import fbchat
 
 def login_to_facebook():
+    with open("config.json", "r") as f:
+        config = json.load(f)
+    
+    email = config["email"]
+    password = config["password"]
+    
     try:
-        with open("appstate.json", "r") as f:
-            session = json.load(f)
-            client = Client(None, None, session_cookies=session)
-    except (FileNotFoundError, Exception):
-        email = "fixxyktimoh@uma3.be"
-        password = "Nota3210@#"
-        try:
-            client = Client(email, password)
-        except fbchat.FBchatUserError as e:
-            print(f"Failed to log in: {e}")
-            return None
-        except Exception as e:
-            print(f"Unexpected error: {e}")
-            return None
-        with open("appstate.json", "w") as f:
-            f.write(json.dumps(client.getSession()))
+        client = Client(email, password)
+    except fbchat.FBchatUserError as e:
+        print(f"Failed to log in: {e}")
+        return None
+    except Exception as e:
+        print(f"Unexpected error: {e}")
+        return None
+    
     return client
