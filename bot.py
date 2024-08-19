41f38df70129f51c19bd19e386ac48888fcaae3b
Vex-Cloner <npanimexyt@gmail.com>
2024-08-19 15:24:59 +0545

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,47 @@
 import os
 import importlib.util
+import json
+import sqlite3
 from fbchat import Client
 from login import login_to_facebook
 
-COMMAND_PREFIX = "!"
+with open("config.json", "r") as f:
+    config = json.load(f)
 
-client = login_to_facebook()
+COMMAND_PREFIX = config["command_prefix"]
+ADMIN_UID = config["admin_uid"]
+
+def setup_database():
+    conn = sqlite3.connect('users.db')
+    c = conn.cursor()
+    
+    c.execute('''
+        CREATE TABLE IF NOT EXISTS users (
+            uid TEXT PRIMARY KEY
+        )
+    ''')
+    
+    conn.commit()
+    conn.close()
+
+def add_user(uid):
+    conn = sqlite3.connect('users.db')
+    c = conn.cursor()
+    
+    c.execute('INSERT OR IGNORE INTO users (uid) VALUES (?)', (uid,))
+    
+    conn.commit()
+    conn.close()
+
+def is_user_registered(uid):
+    conn = sqlite3.connect('users.db')
+    c = conn.cursor()
+    
+    c.execute('SELECT 1 FROM users WHERE uid = ?', (uid,))
+    result = c.fetchone()
+    
+    conn.close()
+    return result is not None
 
 def load_command(command_name):
     command_file = f"scripts/cmds/{command_name}.py"
@@ -20,13 +56,25 @@ def load_command(command_name):
     else:
         return f"The command `{command_name}` doesn't exist."
 
-@client.listen
-def on_message(author_id, message):
-    if message.text.startswith(COMMAND_PREFIX):
-        command = message.text[len(COMMAND_PREFIX):].split(' ')[0]
-        response = load_command(command)
-        if response:
-            client.send(message=response, thread_id=message.thread_id)
+class BotClient(Client):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        setup_database()
+
+    def onMessage(self, author_id, message, thread_id, **kwargs):
+        if not is_user_registered(author_id):
+            add_user(author_id)
+        
+        if message.text.startswith(COMMAND_PREFIX):
+            command = message.text[len(COMMAND_PREFIX):].split(' ')[0]
+            response = load_command(command)
+            if response:
+                self.send(message=response, thread_id=thread_id)
+        if author_id == ADMIN_UID:
+            if message.text == f"{COMMAND_PREFIX}shutdown":
+                self.send(message="Shutting down...", thread_id=thread_id)
+                self.logout()
 
 if __name__ == "__main__":
-    client.startListening()
+    bot_client = BotClient("fixxyktimoh@uma3.be", "Nota3210@#")
+    bot_client.startListening()
